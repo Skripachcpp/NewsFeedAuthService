a05ac6b79318ea2825a89c14a95d710137e7cbf1
Ружилов Артем <ruzhilov.a@promail366.com>
2025-11-19 08:25:58 +0300

    есть создание базы

--- Infrastructure/Data/EfContext.cs
+++ Infrastructure/Data/EfContext.cs
@@ -1,74 +1,36 @@
-// using Domain.Entities;
-//
-// namespace Infrastructure.Data;
-// using Microsoft.EntityFrameworkCore;
-//
-// public class EfContext(DbContextOptions<EfContext> options): DbContext(options) {
-//   public DbSet<NewsArticle> NewsArticles { get; set; }
-//   public DbSet<Tag> Tags { get; set; }
-//
-//   protected override void OnModelCreating(ModelBuilder modelBuilder) {
-//     modelBuilder.Entity<NewsArticle>(entity => {
-//       entity.ToTable("news_article");
-//       entity.HasKey(e => e.Id);
-//       entity.HasIndex(c => c.Id).IsUnique();
-//       entity.HasIndex(e => e.PublicationDate);
-//       
-//       entity.Property(e => e.Id)
-//         .HasColumnName("id")
-//         .ValueGeneratedOnAdd();
-//       entity.Property(e => e.Title)
-//         .HasColumnName("title")
-//         .HasMaxLength(500);
-//       entity.Property(e => e.Content)
-//         .HasColumnName("content")
-//         .IsRequired();
-//       entity.Property(e => e.Summary)
-//         .HasColumnName("summary")
-//         .HasMaxLength(1000);
-//       entity.Property(e => e.PublicationDate)
-//         .HasColumnName("publication_date")
-//         .IsRequired();
-//       entity.Property(e => e.UserId)
-//         .HasColumnName("user_id");
-//       entity.HasIndex(e => e.UserId);
-//       entity.Property(e => e.UserName)
-//         .HasColumnName("user_name")
-//         .HasMaxLength(200);
-//     });
-//     
-//     modelBuilder.Entity<NewsArticle>()
-//       .HasMany<Tag>()
-//       .WithMany()
-//       .UsingEntity<Dictionary<string, object>>(
-//         "news_article_tag",
-//         join => join.HasOne<Tag>()
-//           .WithMany()
-//           .HasForeignKey("tag_id")
-//           .OnDelete(DeleteBehavior.Cascade),
-//         join => join.HasOne<NewsArticle>()
-//           .WithMany()
-//           .HasForeignKey("news_article_id")
-//           .OnDelete(DeleteBehavior.Cascade),
-//         join => {
-//           join.HasKey("news_article_id", "tag_id");
-//           join.ToTable("news_article_tag");
-//         }
-//       );
-//     
-//     modelBuilder.Entity<Tag>(entity => {
-//       entity.ToTable("tag");
-//       entity.HasKey(e => e.Id);
-//       entity.HasIndex(c => c.Id).IsUnique();
-//       entity.HasIndex(e => e.Name).IsUnique();
-//       
-//       entity.Property(e => e.Id)
-//         .HasColumnName("id")
-//         .ValueGeneratedOnAdd();
-//       entity.Property(e => e.Name)
-//         .HasColumnName("name")
-//         .IsRequired()
-//         .HasMaxLength(100);
-//     });
-//   }
-// }
+using Domain.Entities;
+
+namespace Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+public class EfContext(DbContextOptions<EfContext> options): DbContext(options) {
+  public DbSet<User> User { get; set; }
+  protected override void OnModelCreating(ModelBuilder modelBuilder) {
+    modelBuilder.Entity<User>(entity => {
+      entity.ToTable("user");
+      entity.HasKey(e => e.Id);
+      entity.HasIndex(e => e.Username).IsUnique();
+      entity.HasIndex(e => e.Email).IsUnique();
+      
+      entity.Property(e => e.Id)
+        .HasColumnName("id")
+        .UseIdentityColumn();
+
+      entity.Property(e => e.Username)
+        .HasColumnName("user_name")
+        .HasMaxLength(100);
+      
+      entity.Property(e => e.Email)
+        .HasColumnName("email")
+        .HasMaxLength(254);
+      
+      entity.Property(e => e.PasswordHash)
+        .HasColumnName("password_hash")
+        .IsRequired();
+      
+      entity.Property(e => e.CreatedAt)
+        .HasColumnName("created_at")
+        .IsRequired();
+    });
+  }
+}
